a5e1c97217ba5ef24f8c0800739ecb294281a948
Prokashev <prokashev.m.p@gmail.com>
2021-11-26 19:27:15 +0000

    =.

--- task1/progs/c/b1_b2.cpp
+++ task1/progs/c/b1_b2.cpp
@@ -15,53 +15,54 @@ double b2(double j, double N);
 double func(double x);
 
 int main(int argc, char *argv[]) {
-  int b_num = 0; // b1 or b2
-  double j = 0.;
-  double N = 0.;
-  double ret = 0.;
+    int b_num = 0; // b1 or b2
+    double j = 0.;
+    double N = 0.;
+    double ret = 0.;
+    
+    if (argc != 4) {
+        cout << "not enought arg" << endl;
+        return -1;
+    }
+    sscanf(argv[1], "%d", &b_num);
+    sscanf(argv[2], "%lf", &j);
+    sscanf(argv[3], "%lf", &N);
+    
+    switch (b_num) {
+        case 1:
+            ret = b1(j, N);
+            break;
+        case 2: // case 2
+            ret = b2(j, N);
+            break;
+        default:
+            cout << "b_num not 1 or 2" << endl;
+            return -1;
+    }
 
-  if (argc != 4) {
-      cout << "not enought arg";
-      return -1;
-  }
-  sscanf(argv[1], "%d", &b_num);
-  sscanf(argv[2], "%lf", &j);
-  sscanf(argv[3], "%lf", &N);
-
-  switch (b_num) {
-  case 1:
-    ret = b1(j, N);
-    break;
-  case 2:
-    ret = b2(j, N);
-    break;
-  default:
-    return 0;
-  }
-
-  ofstream file(out_path);
-  if (file.is_open()) {
-    file << setprecision(17);
-    file << ret << endl;
+    ofstream file(out_path);
+    if (file.is_open()) {
+        file << setprecision(17);
+        file << ret << endl;
+    } else {
+        cout << "unable to open file" << endl;
+    }
     file.close();
-  } else {
-    cout << "Unable to open file";
-  }
-
-  return 0;
+    
+    return 0;
 }
 
 double b1(double j, double N) {
-  double h = 1. / N;
-  return 4. / 3. * h * func(h * j / 2.);
+    double h = 1. / N;
+    return 4. / 3. * h * func(h * j / 2.);
 }
 
 double b2(double j, double N) {
-  double h = 1. / N;
-  return 2. / 3. * h * func(h * j / 2.);
+    double h = 1. / N;
+    return 2. / 3. * h * func(h * j / 2.);
 }
 
 double func(double x) {
-  return -2 * M_PI * cos(M_PI * x * x) + sin(M_PI * x * x) +
-         sin(M_PI * x * x) * 4 * (M_PI * x) * (M_PI * x);
+    return -2 * M_PI * cos(M_PI * x * x) + sin(M_PI * x * x) +
+        sin(M_PI * x * x) * 4 * (M_PI * x) * (M_PI * x);
 }
--- task1/progs/c/matr.cpp
+++ task1/progs/c/matr.cpp
@@ -18,57 +18,61 @@ const string in_A_path = "in_out/in_A.txt";
 const string in_b_path = "in_out/in_b.txt";
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-      return 0;
-  }
-  int n; // размерность матрицы
-  sscanf(argv[1], "%d", &n);
-  int nrhs = 1;
-  double *a = new double [n*n];
-  double *b = new double [n];
-  int lda = n;
-  int ldb = n;
-  int *ipiv = new int [n];
-  int info;
-  int i, j;
-
-  ifstream file_A(in_A_path);
-  if (file_A.is_open()) {
-    for (i = 0; i < n; i++) {
-      for (j = 0; j < n; j++) {
-        file_A >> a[j*n+i];
-      }
+    if (argc != 2) {
+        return 0;
     }
-    file_A.close();
-  } else {
-    cout << "Unable to open file";
-  }
+    int n; // размерность матрицы
+    sscanf(argv[1], "%d", &n);
+    int nrhs = 1;
+    double *a = new double [n*n];
+    double *b = new double [n];
+    int lda = n;
+    int ldb = n;
+    int *ipiv = new int [n];
+    int info;
+    int i, j;
 
-  ifstream file_b(in_b_path);
-  if (file_b.is_open()) {
-    for (i = 0; i < n; i++) {
-      file_b >> b[i];
-    }
-    file_b.close();
-  } else {
-    cout << "Unable to open file";
-  }
-
-  dgesv_(&n, &nrhs, a, &lda, ipiv, b, &ldb, &info);
-
-  if (info == 0) {
-    ofstream file_c(out_c_path);
-    if (file_c.is_open()) {
-      file_c << setprecision(17);
-      for (i = 0; i < n; i++) {
-        file_c << b[i] << endl;
-      }
-      file_c.close();
+    ifstream file_A(in_A_path);
+    if (file_A.is_open()) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                file_A >> a[j*n+i];
+            }
+        }
+        file_A.close();
     } else {
-      cout << "Unable to open file";
+        cout << "Unable to open file";
     }
-  } else {
-    std::cerr << "dgbsv returned error " << info << "\n";
-  }
-  return info;
+
+    ifstream file_b(in_b_path);
+    if (file_b.is_open()) {
+        for (i = 0; i < n; i++) {
+            file_b >> b[i];
+        }
+        file_b.close();
+    } else {
+        cout << "Unable to open file";
+    }
+
+    dgesv_(&n, &nrhs, a, &lda, ipiv, b, &ldb, &info);
+
+    if (info == 0) {
+        ofstream file_c(out_c_path);
+        if (file_c.is_open()) {
+            file_c << setprecision(17);
+            for (i = 0; i < n; i++) {
+                file_c << b[i] << endl;
+            }
+        } else {
+            cout << "Unable to open file";
+        }
+        file_c.close();
+    } else {
+        std::cerr << "dgbsv returned error " << info << "\n";
+    }
+    delete []a;
+    delete []b;
+    delete []ipiv;
+    
+    return info;
 }
